060a8c88e8a69aede7173d941cb6d8c2e68320b8
MaksStelmakh <step4ick2017@gmail.com>
2021-11-13 12:22:20 +0200

    Прогноз погоды

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,9 @@
 export const getRefs = () => {
-  return { temperatureDescription: document.querySelector('.temperature-description') };
+  return {
+    temperatureDescription: document.querySelector('.temperature-description'),
+    temmperatureDegree: document.querySelector(`.temperature-degree`),
+    locationTimezone: document.querySelector(`.location-timezone`),
+    icon: document.querySelector(`.icon`),
+    iconSkycon: document.querySelector(`#icon1`)
+  };
 };
